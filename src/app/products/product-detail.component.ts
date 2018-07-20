9624c01bd9a5b22f51ea16acca749254b2b0b40e
Orin Walker <owalker@feldinc.com>
2018-07-20 14:52:54 -0400

    finished details page

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { ProductService } from './product.service';
 import { IProduct } from './product';
 
 
@@ -11,24 +12,29 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
+  products: IProduct[];
+  errorMessage: string;
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+            private productService: ProductService) { }
 
   ngOnInit() {
-    // let id = +this.route.snapshot.paramMap.get('id');
     const id = +this.route.snapshot.paramMap.get('id');
     this.pageTitle += `: ${id}`;
-    this.product = {
-      'productId': id,
-      'productName': 'Leaf Rake',
-      'productCode': 'GDN-0011',
-      'releaseDate': 'March 19, 2016',
-      'description': 'Leaf rake with 48-inch wooden handle.',
-      'price': 19.95,
-      'starRating': 3.2,
-      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-    };
+
+    console.log('In OnInit');
+
+        // Set the product property (this.products) to the array of products
+        // that is retrieved from subscribe()
+        // Cast the error returned to the 'any' data-type
+        this.productService.getProducts().subscribe(
+          products => {
+            this.products = products;
+            this.product = products.find(p => p.productId === id);
+          },
+          error => this.errorMessage = <any>error
+        );
   }
 
   onBack() {
